c5ec9f6d870cdb96fa16fef265610ac8df261354
chriskorsak <chris@chriskorsak.com>
2021-04-27 14:22:46 -0700

    refactor and clean up code for clarity and minimizing redundancy.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,11 @@
-//submit form with new item
-document.querySelector('form').addEventListener('submit', function(e) {
-  
-  //prevent form default behavior
-  e.preventDefault();
-  
-  //get input value
-  const input = document.querySelector('input[type="text"]');
-  
-  //create new list element
-  const listItem = document.createElement('li');
-  listItem.textContent = input.value;
+//form input for adding new list item
+const input = document.querySelector('input[type="text"]');
 
-  // create delete button for list item
-  const deleteButton = document.createElement('button');
-  deleteButton.className = 'remove-item';
-  deleteButton.textContent = 'X';
-  listItem.append(deleteButton);
 
-  //add newly created list item to list
-  document.querySelector('ul').append(listItem);
-  //clear out input
-  input.value = '';
-})
-
-//disable submit button until input has text
-
-//disable submit button by default
+//disable submit button by default until input has text
 const submitButton = document.querySelector('input[type="submit"]');
 submitButton.disabled = true;
 
-//check input for text and enable button if text present
-const input = document.querySelector('input[type="text"]');
 input.addEventListener('keyup', function() {
   if (input.value.length > 0) {
     submitButton.disabled = false;
@@ -39,12 +14,39 @@ input.addEventListener('keyup', function() {
   }
 })
 
+//add event when submitting form
+document.querySelector('form').addEventListener('submit', newListItem);
+
+// add a new list item when submitting form
+function newListItem(e) {
+  //prevent form default behavior
+  e.preventDefault();
+  
+  //create text node with input
+  const inputText = document.createTextNode(input.value);
+  
+  //create new list element and add text node
+  const listItem = document.createElement('li');
+  listItem.appendChild(inputText);
+
+  // create delete button for list item
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'remove-item cheboygan';
+  deleteButton.textContent = 'X';
+  listItem.appendChild(deleteButton);
+
+  //add newly created list item to list
+  document.querySelector('ul').append(listItem);
+  //clear out input
+  input.value = '';
+}
+
 //delete list item
 const list = document.querySelector('ul');
 //add click event to entire list
 list.addEventListener('click', function(e) {
   // if the event target classname is 'remove item'...
-  if (e.target.className === 'remove-item') {
+  if (e.target.classList.contains('remove-item')) {
     //select parent element of delete button
     const listItem = e.target.parentElement;
     //remove list item
